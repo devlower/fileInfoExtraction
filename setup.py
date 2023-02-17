90ebe8d3d54c1e351c72c83991027cc7c74b6f61
lucasoal <lucasoalzst@gmail.com>
2023-02-17 08:40:10 -0300

    sort attributes

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name="fileInfoExtraction",
-    version="0.1.0",
     author="Tuanne Assenço",
     author_email="tuanne.assenco@gmail.com",
-    description="A library for retrieving information about files",
-    long_description="A longer description of the library and its features",
-    url="https://github.com/devlower/fileInfoExtraction",
-    packages=["fileInfoExtraction"],
-    install_requires=["chardet", "python-magic"],
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
     ],
+    description="A library for retrieving information about files",
+    install_requires=["chardet", "python-magic"],
+    long_description="A longer description of the library and its features",
+    name="fileInfoExtraction",
+    packages=["fileInfoExtraction"],
+    url="https://github.com/devlower/fileInfoExtraction",
+    version="0.1.0",
 )
